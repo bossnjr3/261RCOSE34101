7776ef39bb213ba0fb00b406b9e2fd329aa1ba33
bossnjr3 <bossnjr3@naver.com>
2026-05-24 20:32:41 +0900

    SJF 스케줄링 시뮬레이션 함수(doSJF)를 추가 힙 기반 ready queue 관리(heap_push, heap_pop)와 남은 burst time 비교 함수(sjf_compare)를 구현

--- scheduler.c
+++ scheduler.c
@@ -35,6 +35,17 @@ int random_int(int min, int max) {
 	return rand() % (max - min + 1) + min;
 }
 
+int sjf_compare(PROCESS* a, PROCESS* b) {
+	int a_time = a->burst_time - a->cpu_used; // 남은 CPU burst time 계산
+	int b_time = b->burst_time - b->cpu_used; // 남은 CPU burst time 계산
+	if (a != b) {
+		return a_time - b_time; // 남은 CPU burst time이 작은 순서대로 정렬
+	}
+	else {
+		return a->pid - b->pid; // 남은 CPU burst time이 같으면 PID가 작은 순서대로 정렬
+	}
+}
+
 PROCESS create_process(int pid) {
 	PROCESS p;
 	p.pid = pid; 
@@ -42,6 +53,12 @@ PROCESS create_process(int pid) {
 	p.burst_time = random_int(1, 20); // CPU burst time을 1에서 20 사이로 랜덤하게 설정
 	p.io_count = random_int(0, MAX_IO); // I/O 요청 수를 1에서 MAX_IO 사이로 랜덤하게 설정
 	p.io_total = 0; // 총 I/O 시간을 초기화
+	// burst가 작으면 IO 넣을 자리가 없으니 io_count 제한
+	int max_io_possible = p.burst_time - 1;   // 1~(burst-1) 범위에 들어갈 수 있는 최대 개수
+	if (p.io_count > max_io_possible) {
+		p.io_count = max_io_possible;
+	}
+	if (p.io_count < 0) p.io_count = 0;   // burst=1이면 max_io_possible=0
 	for (int i = 0; i < p.io_count; i++) {
 		p.io_request_times[i] = random_int(1, p.burst_time - 1); // I/O 요청이 발생하는 시간을 burst time 내에서 랜덤하게 설정
 		p.io_burst_times[i] = random_int(1, 5); // 각 I/O 요청에 필요한 시간을 1에서 5 사이로 랜덤하게 설정
@@ -69,13 +86,17 @@ PROCESS create_process(int pid) {
 	p.state = NEW; // 초기 상태는 NEW
 	return p;
 }
+void heap_push(PROCESS* HEAP[], int size, PROCESS* p);
+PROCESS* heap_pop(PROCESS* HEAP[], int size);
+void doFCFS(PROCESS *original_processes, int p_cnt);
+void doSJF(PROCESS* original_processes, int p_cnt);
+
 
 int main(void) {
 	srand((unsigned int)time(NULL)); // 랜덤 시드 초기화
 	int p_cnt = random_int(3, MAX_PROCESSES); // 생성할 프로세스 수를 3에서 MAX_PROCESSES 사이로 랜덤하게 설정
 	int i, j;
 	PROCESS original_processes[MAX_PROCESSES];
-	PROCESS working_processes[MAX_PROCESSES];
 	for (i = 1; i <= p_cnt; i++) {
 		PROCESS p = create_process(i); // 프로세스 생성
 		printf("Process %d: Arrival Time=%d, Burst Time=%d, IO Count=%d, Priority=%d\n", 
@@ -86,11 +107,19 @@ int main(void) {
 		}
 		original_processes[i - 1] = p; // 원본 프로세스 배열에 저장
 	}		
+	doFCFS(original_processes, p_cnt); // FCFS 스케줄링 시뮬레이션 실행
+	doSJF(original_processes, p_cnt); // SJF 스케줄링 시뮬레이션 실행)
+
+	return 0;
+}
+
+void doFCFS(PROCESS* original_processes, int p_cnt) {
+	PROCESS working_processes[MAX_PROCESSES];
+	int i, j;
 	memcpy(working_processes, original_processes, sizeof(PROCESS) * p_cnt); // 작업용 프로세스 배열에 원본 복사
-	PROCESS* rqueue[MAX_PROCESSES+1]; // READY 상태의 프로세스들을 위한 큐
+	PROCESS* rqueue[MAX_PROCESSES + 1]; // READY 상태의 프로세스들을 위한 큐
 	PROCESS* wqueue[MAX_PROCESSES + 1]; // WAITING 상태의 프로세스들을 위한 큐
 	int head = 0; // 큐에 새 프로세스를 추가할 위치
-	int tail = 0; // 큐에서 프로세스를 꺼낼 위치
 	int whead = 0; // WAITING 큐에 새 프로세스를 추가할 위치
 	int now_cheking = 0; // 현재 실행 중인 프로세스의 인덱스
 	int max_time = 1500; // 시뮬레이션 최대 시간
@@ -118,10 +147,13 @@ int main(void) {
 				j--; // 현재 인덱스에서 다음 프로세스를 확인하기 위해 인덱스 감소
 			}
 		}
-		if (current_process == NULL && head != tail) {
-			current_process = rqueue[tail];
-			tail = (tail + 1) % (MAX_PROCESSES + 1);
-			current_process->state = RUNNING;
+		if (current_process == NULL) {
+
+			current_process = heap_pop(rqueue, head);
+			if (current_process != NULL) {
+				head--;
+				current_process->state = RUNNING;
+			}
 		}
 		// RUNNING 처리
 		if (current_process != NULL) {
@@ -157,10 +189,9 @@ int main(void) {
 			printf("CPU: IDLE            | ");
 
 		printf("READY: ");
-		int r = tail;
-		while (r != head) {
+		int r = 0;
+		for (r = 0; r < head; r++) {
 			printf("P%d ", rqueue[r]->pid);
-			r = (r + 1) % (MAX_PROCESSES + 1);
 		}
 
 		printf("| WAIT: ");
@@ -181,5 +212,154 @@ int main(void) {
 	}
 	printf("Average Waiting Time: %.2f\n", (double)total_wait / p_cnt);
 	printf("Average Turnaround Time: %.2f\n", (double)total_turnaround / p_cnt);
-	return 0;
+}
+
+void doSJF(PROCESS* original_processes, int p_cnt) {
+	PROCESS working_processes[MAX_PROCESSES];
+	int i, j;
+	memcpy(working_processes, original_processes, sizeof(PROCESS) * p_cnt); // 작업용 프로세스 배열에 원본 복사
+	PROCESS* rqueue[MAX_PROCESSES + 1]; // READY 상태의 프로세스들을 위한 큐
+	PROCESS* wqueue[MAX_PROCESSES + 1]; // WAITING 상태의 프로세스들을 위한 큐
+	int head = 0; // 큐에 새 프로세스를 추가할 위치
+	int whead = 0; // WAITING 큐에 새 프로세스를 추가할 위치
+	int now_cheking = 0; // 현재 실행 중인 프로세스의 인덱스
+	int max_time = 1500; // 시뮬레이션 최대 시간
+	int done = 0;
+	PROCESS* current_process = NULL;
+	for (i = 0; i < max_time; i++) {
+		// 시뮬레이션 시간 단위마다 프로세스 상태 업데이트 및 스케줄링 로직 구현
+		for (j = 0; j < p_cnt; j++) {
+			if (working_processes[j].arrival_time == i && working_processes[j].state == NEW) {
+				working_processes[j].state = READY;
+				heap_push(rqueue, head, &working_processes[j]);
+				head++;
+			}
+		}
+		for (j = 0; j != whead; j++) {
+			PROCESS* waiting_process = wqueue[j];
+			waiting_process->io_burst_times[waiting_process->current_io_idx]--;
+			if (waiting_process->io_burst_times[waiting_process->current_io_idx] == 0) {
+				waiting_process->state = READY;
+				heap_push(rqueue, head, waiting_process);
+				waiting_process->current_io_idx++;
+				head++;
+				wqueue[j] = wqueue[whead - 1]; // WAITING 큐에서 제거]
+				whead--;
+				j--; // 현재 인덱스에서 다음 프로세스를 확인하기 위해 인덱스 감소
+			}
+		}
+		if (current_process == NULL) {
+			current_process = heap_pop(rqueue, head);
+			if (current_process != NULL) {
+				head--;
+				current_process->state = RUNNING;
+			}
+		}
+		// RUNNING 처리
+		if (current_process != NULL) {
+			// I/O 요청
+			// I/O 요청 시점은 context switch 비용으로 CPU를 사용하지 않는다고 가정
+			if (current_process->current_io_idx < current_process->io_count &&
+				current_process->cpu_used >= current_process->io_request_times[current_process->current_io_idx]) {
+				current_process->state = WAITING;
+				wqueue[whead] = current_process;
+				whead = (whead + 1) % (MAX_PROCESSES + 1);
+				current_process = NULL;
+			}
+			// 종료
+			else {
+				current_process->cpu_used++;
+				if (current_process->cpu_used == current_process->burst_time) {
+					current_process->state = TERMINATED;
+					current_process->completion_time = i;
+					current_process = NULL;
+					done++;
+					if (done == p_cnt) {
+						break; // 모든 프로세스가 종료되면 시뮬레이션 종료
+					}
+				}
+			}
+		}
+
+		// 매 tick 상태 출력
+		printf("t=%3d | ", i);
+		if (current_process != NULL)
+			printf("CPU: P%d (cpu_used=%d) | ", current_process->pid, current_process->cpu_used);
+		else
+			printf("CPU: IDLE            | ");
+
+		printf("READY: ");
+		int r = 0;
+		while (r != head) {
+			printf("P%d ", rqueue[r]->pid);
+			r = (r + 1) % (MAX_PROCESSES + 1);
+		}
+
+		printf("| WAIT: ");
+		for (j = 0; j < whead; j++)
+			printf("P%d ", wqueue[j]->pid);
+
+		printf("\n");
+	}
+	printf("\n=== SJF result ===\n");
+	int total_wait = 0, total_turnaround = 0;
+	for (j = 0; j < p_cnt; j++) {
+		int turnaround = working_processes[j].completion_time - working_processes[j].arrival_time;
+		int waiting = turnaround - working_processes[j].burst_time - working_processes[j].io_total;
+		total_wait += waiting;
+		total_turnaround += turnaround;
+		printf("P%d: completion=%d, turnaround=%d, waiting=%d\n",
+			working_processes[j].pid, working_processes[j].completion_time, turnaround, waiting);
+	}
+	printf("Average Waiting Time: %.2f\n", (double)total_wait / p_cnt);
+	printf("Average Turnaround Time: %.2f\n", (double)total_turnaround / p_cnt);
+}
+
+
+void heap_push(PROCESS* HEAP[], int size, PROCESS* p) {
+	HEAP[size] = p;
+	int i = size;
+	int parent;
+	while (i > 0) {
+		parent = (i - 1) / 2;
+		if (sjf_compare(HEAP[i], HEAP[parent]) < 0) {
+			PROCESS* temp = HEAP[i];
+			HEAP[i] = HEAP[parent];
+			HEAP[parent] = temp;
+			i = parent;
+		}
+		else {
+			break;
+		}
+	}
+}
+
+PROCESS* heap_pop(PROCESS* HEAP[], int size) {
+	if (size == 0) return NULL;
+	PROCESS* top = HEAP[0];
+	HEAP[0] = HEAP[size - 1];
+	int i = 0;
+	int left, right, smallest;
+	size--;
+	while (1) {
+		left = 2 * i + 1;
+		right = 2 * i + 2;
+		smallest = i;
+		if (left < size && sjf_compare(HEAP[left], HEAP[smallest]) < 0) {
+			smallest = left;
+		}
+		if (right < size && sjf_compare(HEAP[right], HEAP[smallest]) < 0) {
+			smallest = right;
+		}
+		if (smallest != i) {
+			PROCESS* temp = HEAP[i];
+			HEAP[i] = HEAP[smallest];
+			HEAP[smallest] = temp;
+			i = smallest;
+		}
+		else {
+			break;
+		}
+	}
+	return top;
 }
